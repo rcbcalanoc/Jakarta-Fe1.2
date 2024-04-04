abc6ec339784ed30aa696cdbb677276334941608
rcbcalanoc <richardbcalanoc@gmail.com>
2024-04-05 01:22:41 +0800

    Page 1 added

---
+++ src/components/pages/page1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page1() {
+  return (
+    <div className="page-container">
+      Richard Carlos B. Calanoc SN:2021107332
+    </div>
+  );
+}
+
+export default Page1;
