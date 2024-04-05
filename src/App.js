9b59663f97676cf623495a3dc2969b85903c78b4
rcbcalanoc <richardbcalanoc@gmail.com>
2024-04-05 14:46:44 +0800

    compilation error fixed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Template from "./components/Template";
 import Nav from "./components/navbar/Nav";
 import Calanoc from "./components/members/calanoc/Calanoc";
-import Page2 from "./components/members/delapena/Dela Pena";
-import Page3 from "./components/members/magbanua/Magbanua";
-import Page4 from "./components/members/seballos/Seballos";
-import Page5 from "./components/members/sy/Sy";
+import Page2 from "./components/members/surname2/Page2";
+import Page3 from "./components/members/surname3/Page3";
+import Page4 from "./components/members/surname4/Page4";
+import Page5 from "./components/members/surname5/Page5";
 import Vallesteros from "./components/members/vallesteros/Vallesteros";
 
 import './App.css';
@@ -25,11 +25,11 @@ function App() {
         {/* Using the Routes and Route components from react-router-dom */}
         <Routes>
           <Route path="/" element={<Template />}>
-            <Route path="/Calanoc" element={<Calanoc />} />
-            <Route path="/delapena" element={<Dela Pena />} />
-            <Route path="/magbanua" element={<Magbanua />} />
-            <Route path="/seballos" element={<Seballos />} />
-            <Route path="/sy" element={<Sy />} />
+            <Route path="/calanoc" element={<Calanoc />} />
+            <Route path="/surname2" element={<Page2 />} />
+            <Route path="/surname3" element={<Page3 />} />
+            <Route path="/surname4" element={<Page4 />} />
+            <Route path="/surname5" element={<Page5 />} />
             <Route path="/vallesteros" element={<Vallesteros />} />
           </Route>
         </Routes>
